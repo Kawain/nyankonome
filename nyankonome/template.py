1aede648c1f895a9eaf2cb984a655ad32af74d72
unknown <unknown@example.com>
2019-06-01 22:07:49 +0900

    テンプレート変更

--- nyankonome/template.py
+++ nyankonome/template.py
@@ -1,88 +1,118 @@
 # <span v-html="explain2"></span> ここを置換
-template = """<center>
-<table width="550" cellspacing="1" cellpadding="0" border="0" bgcolor="#FFE4E1">
-<tbody>
-<tr>
-<td bgcolor="#ffffff">
-<table width="550" cellspacing="0" cellpadding="0" border="0">
-<tbody>
-<tr>
-<td width="550" bgcolor="#ffffff"><img src="http://www.booboo.ne.jp/cgi-bin/cgisam/btm/flower/flower02.jpg">
-</td>
-</tr>
-<tr>
-<td bgcolor="#ffffff" align="center"> <br><br>
-<table width="450" border="0" bgcolor="#ffffff">
-<tbody>
-<tr>
-<td align="left">
-<font size="3" color="#ffcccc"><b>☆</b></font>
-<font size="3" color="#ff6666"><b>商品詳細☆</b></font>
-</td>
-</tr>
-<tr>
-<td align="left">
-<font size="3" color="#ff6666"><span v-html="explain2"></span></font>
-</td>
-</tr>
-<tr>
-<td align="left">
-<font size="3" color="#ffcccc"><b>☆</b></font>
-<font size="3" color="#ff6666"><b>発送について☆</b></font>
-</td>
-</tr>
-<tr>
-<td align="left">
-<font size="3" color="#ff6666">おまとめ大歓迎です☆お安い方法をお調べします☆</font>
-<br><br><br>
-</td>
-</tr>
-<tr>
-<td align="left">
-<font size="3" color="#ffcccc"><b>☆</b></font>
-<font size="3" color="#ff6666"><b>お願いがあります☆</b></font>
-</td>
-</tr>
-<tr>
-<td align="left">
-<font size="3" color="#ff6666">
-急なお知らせなどありますので、自己紹介欄を必ずご覧下さい。<br><br>パソコンで、作業をしています。<br>レスポンスよくお返事や発送はできません。ごめんなさい。<br><br>
-丁寧な梱包を心がけますが、過剰な梱包はしておりません。<br><br>土曜・日曜・祝日はメッセージ・発送などはお休みします。<br><br>見落としてる汚れなどあるかもしれません。<br>
-リサイクル品にご理解のある方お願いします☆<br><br>実寸は平置きで計ったものです。微妙に変わることがあります。<br>色についても出来るだけ詳しく書きますが、微妙な感じ方にご理解ください。<br><br>
-こちらからの評価は、忙しくできない状態です！ごめんなさい！<br>必要な場合はお知らせください。
-</font> <br><br><br>
-</td>
-</tr>
-<tr>
-<td align="left">
-<font size="3" color="#ff6666"><b>◇お店紹介◇</b></font>
-</td>
-</tr>
-<tr>
-<td align="left">
-<font size="3" color="#ff6666">
-nyankonomeonlineは子供服のリサイクルショップです。<br><br>これからドンドンお洋服を出品していきます！<br>どうぞよろしくお願いします。<br><br>
-お近くの方は手渡し可能です☆相談してください☆<br><br>子供服のリサイクルにゃんこのめ<br>住所：北海道札幌市白石区<br>古物商番号：第 101020001085 号<br>
-交付年月日： 平成18年4月25日 <br>交付者： 北海道公安委員会<br><br>ブランド子供服の買取いたします☆<br>たんすや押入れに眠っているものはありませんか？？<br>
-季節を問わずオールシーズンＯＫです。<br>ブランド品はもちろん、きれいなノーブランドでも買取しますよ。<br><br>子供服にゃんこのめと検索☆</font> <br><br><br>
-</td>
-</tr>
-</tbody>
-</table> <br><br>
-<div></div>
-</td>
-</tr>
-<tr>
-<td width="550" bgcolor="#ffffff"><img src="http://www.booboo.ne.jp/cgi-bin/cgisam/btm/flower/flower02b.jpg">
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table> <br><a href="https://auctions.yahoo.co.jp/jp/show/bouncer?url=http%3A%2F%2Fwww.booboo.ne.jp%2Fcgi-bin%2Fcgisam%2Fbtm%2Findex.html&amp;crumb=vUF5RBLy0piB0qfVTJBC5aWqGX2NxvQRFlq%2F9te95OSe4KQiIa1G45Mi4Xdu2dBfJc%2FmFNqrHPHDe0w1RAnjcw%3D%3D" target="new">
-<font size="1" color="#a5a5a5">//Boo Template Maker Ver1.25//</font>
-</a>
-</center>
+template = """<BR>
+<CENTER>
+<FONT SIZE=5 COLOR=#339900><B> </B></FONT> <BR><BR>
+<TABLE WIDTH=600 CELLSPACING=10 CELLPADDING=10 BORDER=0>
+<TR>
+<TD WIDTH=10% BGCOLOR=#339900 ALIGN=center>
+<FONT SIZE=4 COLOR=#FF0000>★</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#66EE00 ALIGN=center>
+<FONT SIZE=2 COLOR=#66EE00>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#66EE00 ALIGN=center>
+<FONT SIZE=1 COLOR=#FF0000>★</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#FF0000 ALIGN=center>
+<FONT SIZE=1 COLOR=#66EE00>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#66EE00 ALIGN=center>
+<FONT SIZE=2 COLOR=#66EE00>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#339900 ALIGN=center>
+<FONT SIZE=1 COLOR=#339900>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#66EE00 ALIGN=center>
+<FONT SIZE=4 COLOR=#FF0000>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#FF0000 ALIGN=center>
+<FONT SIZE=4 COLOR=#FF0000>★</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#FF0000 ALIGN=center>
+<FONT SIZE=2 COLOR=#66EE00>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#FF0000 ALIGN=center>
+<FONT SIZE=1 COLOR=#339900>★</FONT>
+</TD>
+</TR>
+<TR>
+<TD WIDTH=10% BGCOLOR=#FF0000 ALIGN=center>
+<FONT SIZE=1 COLOR=#339900>★</FONT>
+</TD>
+<TD COLSPAN=8 ROWSPAN=3 BGCOLOR=#CCFFCC ALIGN=left>
+<FONT SIZE=3 COLOR=#339900><span v-html="explain2"></span>
+<BR> <BR> ☆ 発送について☆<BR> おまとめ大歓迎です☆お安い方法をお調べします☆ <BR> <BR> <BR> ☆ お願いがあります☆<BR>
+急なお知らせなどありますので、自己紹介欄を必ずご覧下さい。<BR> <BR> パソコンで、作業をしています。<BR> レスポンスよくお返事や発送はできません。ごめんなさい。<BR> <BR>
+丁寧な梱包を心がけますが、過剰な梱包はしておりません。<BR> <BR> 土曜・日曜・祝日はメッセージ・発送などはお休みします。<BR> <BR>
+見落としてる汚れなどあるかもしれません。<BR> リサイクル品にご理解のある方お願いします☆<BR> <BR> 実寸は平置きで計ったものです。微妙に変わることがあります。<BR>
+色についても出来るだけ詳しく書きますが、微妙な感じ方にご理解ください。<BR> <BR> こちらからの評価は、忙しくできない状態です！ごめんなさい！<BR> 必要な場合はお知らせください。
+<BR> <BR> <BR> ◇お店紹介◇<BR> nyankonomeonlineは子供服のリサイクルショップです。<BR> <BR> これからドンドンお洋服を出品していきます！<BR>
+どうぞよろしくお願いします。<BR> <BR> お近くの方は手渡し可能です☆相談してください☆<BR> <BR> 子供服のリサイクルにゃんこのめ<BR> 住所：北海道札幌市白石区<BR>
+古物商番号： 第 101020001085 号<BR> 交付年月日： 平成18年4月25日 <BR> 交付者： 北海道公安委員会 <BR> <BR> ブランド子供服の買取いたします☆<BR>
+たんすや押入れに眠っているものはありませんか？？<BR> 季節を問わずオールシーズンＯＫです。<BR> ブランド品はもちろん、きれいなノーブランドでも買取しますよ。<BR> <BR>
+子供服にゃんこのめ と検索☆ <BR> </FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#339900 ALIGN=center>
+<FONT SIZE=2 COLOR=#66EE00>★</FONT>
+</TD>
+</TR>
+<TR>
+<TD WIDTH=10% BGCOLOR=#339900 ALIGN=center>
+<FONT SIZE=4 COLOR=#66EE00>★</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#66EE00 ALIGN=center>
+<FONT SIZE=3 COLOR=#66EE00>☆</FONT>
+</TD>
+</TR>
+<TR>
+<TD WIDTH=10% BGCOLOR=#339900 ALIGN=center>
+<FONT SIZE=4 COLOR=#66EE00>★</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#66EE00 ALIGN=center>
+<FONT SIZE=3 COLOR=#66EE00>☆</FONT>
+</TD>
+</TR>
+<TR>
+<TD WIDTH=10% BGCOLOR=#339900 ALIGN=center>
+<FONT SIZE=4 COLOR=#FF0000>★</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#66EE00 ALIGN=center>
+<FONT SIZE=2 COLOR=#66EE00>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#66EE00 ALIGN=center>
+<FONT SIZE=1 COLOR=#FF0000>★</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#FF0000 ALIGN=center>
+<FONT SIZE=1 COLOR=#66EE00>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#66EE00 ALIGN=center>
+<FONT SIZE=2 COLOR=#66EE00>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#339900 ALIGN=center>
+<FONT SIZE=1 COLOR=#339900>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#66EE00 ALIGN=center>
+<FONT SIZE=4 COLOR=#FF0000>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#FF0000 ALIGN=center>
+<FONT SIZE=4 COLOR=#FF0000>★</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#FF0000 ALIGN=center>
+<FONT SIZE=2 COLOR=#66EE00>☆</FONT>
+</TD>
+<TD WIDTH=10% BGCOLOR=#FF0000 ALIGN=center>
+<FONT SIZE=1 COLOR=#339900>★</FONT>
+</TD>
+</TR>
+</TABLE><BR> <SMALL>ほかにも出品しています。よろしければご覧ください<BR></SMALL><A
+HREF=http://auctions.yahoo.co.jp/jp/booth/nyankonomeonline TARGET=new><IMG
+SRC=https://s.yimg.jp/images/auct/front/button01.gif WIDTH=120 HEIGHT=90 BORDER=0></A><BR><BR>
+<FONT SIZE=1 COLOR=#999999>+ + +&nbsp;この商品説明は</FONT> <A
+HREF=https://auctions.yahoo.co.jp/jp/show/bouncer?url=http%3A%2F%2Fwww.auclinks.com%2F&crumb=wMUlvNqcz9rGLmhyHVtKEHybaedkO55qljnk7zNCb9vIHHCyXm3YKAcE2n8XX5csoiBlUyGJdB6uNq9%2F%2Fc4z%2Bg%3D%3D
+TARGET=new>
+<FONT SIZE=1 COLOR=#666666>オークションプレートメーカー２</FONT>
+</A>
+<FONT SIZE=1 COLOR=#999999>で作成しました &nbsp;+ + +</FONT>
+<FONT SIZE=1 COLOR=#FFFFFF><BR>No.102.001.008</FONT><BR>
+</CENTER>
 """
